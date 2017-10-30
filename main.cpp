989486f3f35dccacf0587268eebc704738a12d7f
Hirschmensch <michael.hirschmugl@gmx.at>
2017-10-30 21:18:28 +0100

    Add files via upload
    Working initial version of the whole project.

---
+++ main.cpp
@@ -0,0 +1,317 @@
+#include <stdint.h>
+#include "intrinsics.h"
+#include "sam3x8e.h"
+
+#define NCoef 2
+#define DCgain_BP 192   //64*3
+#define DCgain_TP 384   //128*3
+#define DCgain_HP 3     //1*3
+#define DCgain_MainTP 1
+
+using namespace std;
+
+static int lrtoggle = 0;
+
+static int32_t input_l = 0;
+
+static int32_t input_l_x_BP[NCoef+1];
+static int32_t input_l_y_BP[NCoef+1];
+static int32_t input_l_x_TP[NCoef+1];
+static int32_t input_l_y_TP[NCoef+1];
+static int32_t input_l_x_HP[NCoef+1];
+static int32_t input_l_y_HP[NCoef+1];
+static int32_t input_l_HP_temp = 0;
+
+static int32_t input_l_x_MainTP[NCoef+1];
+static int32_t input_l_y_MainTP[NCoef+1];
+
+static int32_t input_r = 0;
+
+static int32_t input_r_x_BP[NCoef+1];
+static int32_t input_r_y_BP[NCoef+1];
+static int32_t input_r_x_TP[NCoef+1];
+static int32_t input_r_y_TP[NCoef+1];
+static int32_t input_r_x_HP[NCoef+1];
+static int32_t input_r_y_HP[NCoef+1];
+static int32_t input_r_HP_temp = 0;
+
+static int32_t input_r_x_MainTP[NCoef+1];
+static int32_t input_r_y_MainTP[NCoef+1];
+
+static int8_t input_pointer[NCoef+1];
+
+static int32_t vol_pot = 0;
+static uint8_t mute = 0;
+static uint32_t mute_count = 0;
+
+static int32_t ACoef_BP[NCoef+1] =
+{
+    76,
+    0,
+    -76
+};
+
+static int32_t BCoef_BP[NCoef+1] =
+{
+    64,
+    -125,
+    62
+};
+
+static int32_t ACoef_TP[NCoef+1] =
+{
+    32,
+    64,
+    32
+};
+
+static int32_t BCoef_TP[NCoef+1] =
+{
+    64,
+    -116,
+    53
+};
+
+static int32_t ACoef_HP[NCoef+1] =
+{
+    58,
+    -116,
+    58
+};
+
+static int32_t BCoef_HP[NCoef+1] =
+{
+    64,
+    -116,
+    53
+};
+
+static int32_t ACoef_MainTP[NCoef+1] =
+{
+    44,
+    88,
+     44
+};
+
+static int32_t BCoef_MainTP[NCoef+1] =
+{
+    64,
+    81,
+    30
+};
+
+class SSC1
+{
+public:
+    static void Handler();
+    static volatile unsigned int ticks;
+};
+
+void SSC1::Handler()
+{
+    uint8_t i = 0;
+
+    if((PIOD_PDSR & 0x80U) == 0x80U)
+    {
+        mute_count++;
+    }
+    else
+    {
+        mute_count = 0;
+    }
+    if(mute_count > 0xFFFFU)
+    {
+        mute_count = 0;
+        if(mute == 1) mute = 0;
+        else mute = 1;
+    }
+
+    if(!lrtoggle)
+    {
+        lrtoggle = 1;
+        
+        input_l = (((int32_t)(SSC_RHR << 8)));
+        input_l = input_l / 256;
+
+        vol_pot = ADC_CDR;
+
+        i = 0;
+        while(i <= NCoef)
+        {
+            if((input_pointer[i]) < NCoef) input_pointer[i]++;
+            else input_pointer[i] = 0;
+            i++;
+        }
+        
+        input_l_x_TP[input_pointer[0]] = input_l;
+        input_l_y_TP[input_pointer[0]] = ACoef_TP[0] * input_l;
+        input_l_x_BP[input_pointer[0]] = input_l;
+        input_l_y_BP[input_pointer[0]] = ACoef_BP[0] * input_l;
+        input_l_x_HP[input_pointer[0]] = input_l;
+        input_l_y_HP[input_pointer[0]] = ACoef_HP[0] * input_l;
+        
+        i = 1;
+        while(i <= NCoef)
+        {
+            input_l_y_TP[input_pointer[0]] =
+                input_l_y_TP[input_pointer[0]] +
+                (ACoef_TP[i] * input_l_x_TP[input_pointer[i]]) -
+                (BCoef_TP[i] * input_l_y_TP[input_pointer[i]]);
+            input_l_y_BP[input_pointer[0]] =
+                input_l_y_BP[input_pointer[0]] +
+                (ACoef_BP[i] * input_l_x_BP[input_pointer[i]]) -
+                (BCoef_BP[i] * input_l_y_BP[input_pointer[i]]);
+            input_l_y_HP[input_pointer[0]] =
+                input_l_y_HP[input_pointer[0]] +
+                (ACoef_HP[i] * input_l_x_HP[input_pointer[i]]) -
+                (BCoef_HP[i] * input_l_y_HP[input_pointer[i]]);
+            i++;
+        }
+        
+        input_l_y_TP[input_pointer[0]] =
+            input_l_y_TP[input_pointer[0]] / BCoef_TP[0];
+        input_l_y_BP[input_pointer[0]] =
+            input_l_y_BP[input_pointer[0]] / BCoef_BP[0];
+        input_l_y_HP[input_pointer[0]] =
+            input_l_y_HP[input_pointer[0]] / BCoef_HP[0];
+        
+        input_l_HP_temp =
+            ((input_l_y_HP[input_pointer[0]] * 256) / DCgain_HP);
+        
+        if(vol_pot != 0) input_l_HP_temp = input_l_HP_temp / (vol_pot/10);
+        input_l =
+            ((input_l_y_TP[input_pointer[0]] * 256) / DCgain_TP) +
+            ((input_l_y_BP[input_pointer[0]] * 256) / DCgain_BP) +
+            input_l_HP_temp;
+        
+        
+        input_l_x_MainTP[input_pointer[0]] = (input_l / 256);
+        input_l_y_MainTP[input_pointer[0]] = ACoef_MainTP[0] * input_l_x_MainTP[input_pointer[0]];
+        
+        i = 1;
+        while(i <= NCoef)
+        {
+            input_l_y_MainTP[input_pointer[0]] =
+                input_l_y_MainTP[input_pointer[0]] +
+                (ACoef_MainTP[i] * input_l_x_MainTP[input_pointer[i]]) -
+                (BCoef_MainTP[i] * input_l_y_MainTP[input_pointer[i]]);
+            i++;
+        }
+        
+        input_l_y_MainTP[input_pointer[0]] =
+            input_l_y_MainTP[input_pointer[0]] / BCoef_MainTP[0];
+        
+        input_l = ((input_l_y_MainTP[input_pointer[0]] * 128) / DCgain_MainTP);
+        
+        if(mute) SSC_THR = 0;
+        else SSC_THR = input_l;
+    }
+    else
+    {
+        lrtoggle = 0;
+        
+        input_r = (((int32_t)(SSC_RHR << 8)));
+        input_r = input_r / 256;
+
+        input_r_x_TP[input_pointer[0]] = input_r;
+        input_r_y_TP[input_pointer[0]] = ACoef_TP[0] * input_r;
+        input_r_x_BP[input_pointer[0]] = input_r;
+        input_r_y_BP[input_pointer[0]] = ACoef_BP[0] * input_r;
+        input_r_x_HP[input_pointer[0]] = input_r;
+        input_r_y_HP[input_pointer[0]] = ACoef_HP[0] * input_r;
+        
+        i = 1;
+        while(i <= NCoef)
+        {
+            input_r_y_TP[input_pointer[0]] =
+                input_r_y_TP[input_pointer[0]] +
+                (ACoef_TP[i] * input_r_x_TP[input_pointer[i]]) -
+                (BCoef_TP[i] * input_r_y_TP[input_pointer[i]]);
+            input_r_y_BP[input_pointer[0]] =
+                input_r_y_BP[input_pointer[0]] +
+                (ACoef_BP[i] * input_r_x_BP[input_pointer[i]]) -
+                (BCoef_BP[i] * input_r_y_BP[input_pointer[i]]);
+            input_r_y_HP[input_pointer[0]] =
+                input_r_y_HP[input_pointer[0]] +
+                (ACoef_HP[i] * input_r_x_HP[input_pointer[i]]) -
+                (BCoef_HP[i] * input_r_y_HP[input_pointer[i]]);
+            i++;
+        }
+        
+        input_r_y_TP[input_pointer[0]] =
+            input_r_y_TP[input_pointer[0]] / BCoef_TP[0];
+        input_r_y_BP[input_pointer[0]] =
+            input_r_y_BP[input_pointer[0]] / BCoef_BP[0];
+        input_r_y_HP[input_pointer[0]] =
+            input_r_y_HP[input_pointer[0]] / BCoef_HP[0];
+        
+        input_r_HP_temp =
+            ((input_r_y_HP[input_pointer[0]] * 256) / DCgain_HP);
+        
+        if(vol_pot != 0) input_r_HP_temp = input_r_HP_temp / (vol_pot/10);
+        input_r =
+            ((input_r_y_TP[input_pointer[0]] * 256) / DCgain_TP) +
+            ((input_r_y_BP[input_pointer[0]] * 256) / DCgain_BP) +
+            input_r_HP_temp;
+        
+        
+        input_r_x_MainTP[input_pointer[0]] = (input_r / 256);
+        input_r_y_MainTP[input_pointer[0]] = ACoef_MainTP[0] * input_r_x_MainTP[input_pointer[0]];
+        
+        i = 1;
+        while(i <= NCoef)
+        {
+            input_r_y_MainTP[input_pointer[0]] =
+                input_r_y_MainTP[input_pointer[0]] +
+                (ACoef_MainTP[i] * input_r_x_MainTP[input_pointer[i]]) -
+                (BCoef_MainTP[i] * input_r_y_MainTP[input_pointer[i]]);
+            i++;
+        }
+        
+        input_r_y_MainTP[input_pointer[0]] =
+            input_r_y_MainTP[input_pointer[0]] / BCoef_MainTP[0];
+        
+        input_r = ((input_r_y_MainTP[input_pointer[0]] * 128) / DCgain_MainTP);
+        
+        if(mute) SSC_THR = 0;
+        else SSC_THR = input_r;
+    }
+}
+
+int main()
+{
+    int i = NCoef;
+    int j = 0;
+
+    while(i >= 0)
+    {
+        input_pointer[i] = j;
+        j++;
+        i--;
+    }
+
+    __disable_interrupt();
+
+    SAM3X8E.disable_watchdog_timer();
+
+    SAM3X8E.init_clock();
+    SAM3X8E.init_clock_for_wm8731();
+    SAM3X8E.init_twi1();
+    SAM3X8E.setup_twi1_master_transfer();
+    SAM3X8E.init_ssc();
+
+    i =  SAM3X8E.setup_WM8731();
+
+    SAM3X8E.setup_ssc_master_transfer();
+    SAM3X8E.ssc_interrupt_setup();
+    SAM3X8E.enable_digital_input();
+    SAM3X8E.enable_adc_input();
+
+    __enable_interrupt();
+
+    while(1)
+    {
+
+    }
+
+}
---
+++ sam3x8e.cpp
@@ -0,0 +1,648 @@
+/*
+  sam3x8e.cpp
+*/
+
+#include "sam3x8e.h"
+#include "intrinsics.h"
+
+/*
+    Enables the external crystal oscillator.
+*/
+void SAM3X8EClass::enable_main_oscillator(void)
+{
+    PMC_WPMR = WPKEY | WPEN_0;
+    CKGR_MOR = KEY | MOSCXTEN | MOSCXTBY | MOSCRCEN | MOSCRCF | MOSCXTST 
+                   | MOSCSEL | CFDEN;
+    PMC_WPMR = WPKEY | WPEN_1;
+}
+
+/*
+    Waiting for the external crystal oscillator to lock into it's
+    new frequency.
+    1: timeout
+    0: OK
+*/
+uint8_t SAM3X8EClass::wait_for_main_oscillator(void)
+{
+    uint16_t i = 0;
+    
+    while(MOSCXTS != (PMC_SR & MOSCXTS))
+    {
+        if(i++ > GLOBAL_TIMEOUT)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+/*
+    Multiplies external clock frequency times 14.
+    12MHz * 14 = 168MHz
+*/
+void SAM3X8EClass::set_pll_and_divider(void)
+{
+    PMC_WPMR = WPKEY | WPEN_0;
+    CKGR_PLLAR = DIVA | PLLACOUNT | MULA | ONE;
+    PMC_WPMR = WPKEY | WPEN_1;
+}
+
+/*
+    Waiting for the pll to lock into it's new frequency.
+    1: timeout
+    0: OK
+*/
+uint8_t SAM3X8EClass::wait_for_pll(void)
+{
+    uint16_t i = 0;
+    
+    while(LOCKA != (PMC_SR & LOCKA))
+    {
+        if(i++ > GLOBAL_TIMEOUT)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+/*
+    Prescaler divides the pll by factor 2.
+    168MHz / 2 = 84MHz
+    CSS selects this new clock as master clock.
+    1: timeout occured.
+    0: OK
+*/
+uint8_t SAM3X8EClass::select_master_clock(void)
+{
+    PMC_WPMR = WPKEY | WPEN_0;
+    PMC_MCKR = (PMC_MCKR & ~PRES_MSK) | PRES;
+    if(wait_for_master_clock())
+    {
+        return 1;
+    }
+    PMC_MCKR = (PMC_MCKR & ~CSS_MSK) | CSS;
+    if(wait_for_master_clock())
+    {
+        return 1;
+    }
+    PMC_WPMR = WPKEY | WPEN_1;
+    return 0;
+}
+
+/*
+    Waiting for the master clock to lock into it's new frequency.
+    1: timeout
+    0: OK
+*/
+uint8_t SAM3X8EClass::wait_for_master_clock(void)
+{
+    uint16_t i = 0;
+  
+    while(MCKRDY != (PMC_SR & MCKRDY))
+    {
+        if(i++ > GLOBAL_TIMEOUT)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+/*
+    Reads the main oscillator frequency and stores the result in
+    main_oscillator_frequency.
+*/
+void SAM3X8EClass::check_main_oscillator_frequency(void)
+{
+    while(MAINFRDY != (CKGR_MCFR & MAINFRDY)){}  // This should be ready by now
+    main_oscillator_frequency = (((CKGR_MCFR & MAINF_MSK) / 16) * 32000);
+}
+
+/*
+    This is needed because the new clock frequency might be too high for the
+    flash.
+*/
+void SAM3X8EClass::change_flash_read_waitstate(int waitstate)
+{
+    EEFC_FMR0 = (waitstate << 8);
+    EEFC_FMR1 = (waitstate << 8);
+}
+
+/*
+    Sets the main clock frequency to 84MHz.
+    1: NOK (timeout)
+    0: OK
+*/
+uint8_t SAM3X8EClass::init_clock(void)
+{
+    uint8_t i = 0;
+    
+    change_flash_read_waitstate(5);
+    enable_main_oscillator();
+    i |= (wait_for_main_oscillator() << 0);
+    set_pll_and_divider();
+    i |= (wait_for_pll() << 1);
+    i |= (select_master_clock() << 2);
+    check_main_oscillator_frequency();
+    
+    if(i > 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+/*
+    Enables the peripheral clock for TWI1.
+*/
+void SAM3X8EClass::init_twi1_clk(void)
+{
+    PMC_WPMR = 0x504D4300U;
+    PMC_PCER0 = PMC_PCER0 | TWI1_PER;
+    PMC_WPMR = 0x504D4301U;
+}
+
+/*
+    Initializes the TWI1 controller.
+*/
+void SAM3X8EClass::init_twi1(void)
+{
+    init_twi1_clk();
+    setup_pio_for_twi1();
+}
+
+/*
+    The two pins for TWI1 are configured.
+    Peripheral control is disabled and peripheral function A is chosen.
+*/
+void SAM3X8EClass::setup_pio_for_twi1(void)
+{
+    PIOB_WPMR = (0x50494FU << 8) | (0U << 0);
+    PIOB_PDR = (1U << 12);
+    PIOB_PDR = (1U << 13);
+    PIOB_ABSR = (0U << 12);
+    PIOB_ABSR = (0U << 13);
+    PIOB_WPMR = (0x50494FU << 8) | (1U << 0);
+}
+
+/*
+    Configures TWI1 for master mode transfer.
+*/
+void SAM3X8EClass::setup_twi1_master_transfer(void)
+{
+    TWI1_CR = SWRST;  // Reset
+    /*
+        CLDIV, CHDIV and CKDIV values are copied from ASF.
+    */
+    TWI1_CWGR = CLDIV | CHDIV | CKDIV;
+    TWI1_CR = MSEN | SVDIS;  // enables master mode
+    /*
+        enables master write mode.
+        Also, DADR is the adress of the device the communication
+        will go to.
+        The WM8731 has a hardwired adress of 0x1A.
+    */
+    TWI1_MMR = MREAD | DADR;
+}
+
+
+/*
+    Initialization of clock output for WM8731 chip
+*/
+void SAM3X8EClass::init_clock_for_wm8731(void)
+{
+    PIOA_WPMR = (0x50494FU << 8) | (0U << 0);
+    PIOA_PDR = ~PIOA_PSR | (1U << 1);  // enable peripheral control
+    PIOA_PER = PIOA_PSR & ~(1U << 1);
+    PIOA_ABSR |= (1U << 1);  // peripherial B selected
+    PIOA_WPMR = (0x50494FU << 8) | (1U << 0);
+    
+    PMC_WPMR = 0x504D4300U;
+    PMC_SCER |=	0x0100U;  // page 558
+    PMC_PCK0 = 0x01U;  // main crystak osc = 12Mhz
+    PMC_WPMR = 0x504D4301U;
+}
+
+uint16_t SAM3X8EClass::setup_WM8731(void)
+{
+    uint16_t i = 0;
+    
+    i |= (send_two_bytes_to_wm8731(0x0CU, ((PWROFF << 7)|(CLKOUTPUT << 6)
+                                    |(OSCPD << 5)
+                                    |(OUTPD << 4)|(DACPD << 3)|(ADCPD << 2)
+                                    |(MICPD << 1)|(LINEINPD << 0))) << 0 );
+    i |= (send_two_bytes_to_wm8731(0x0EU, ((BCLKINV << 7)|(MS << 6)
+                                    |(LRSWAP << 5)
+                                    |(LRP << 4)|(IWL << 2)
+                                    |(FORMAT << 0))) << 1);
+    i |= (send_two_bytes_to_wm8731(0x00U, ((LRINBOTH << 8)|(LINMUTE << 7)
+                                    |(LINVOL << 0))) << 2);
+    i |= (send_two_bytes_to_wm8731(0x02U, ((RLINBOTH << 8)|(RINMUTE << 7)
+                                    |(RINVOL << 0))) << 3);
+    i |= (send_two_bytes_to_wm8731(0x04U, ((LRHPBOTH << 8)|(LZCEN << 7)
+                                    |(LHPVOL << 0))) << 4);
+    i |= (send_two_bytes_to_wm8731(0x06U, ((RLHPBOTH << 8)|(RZCEN << 7)
+                                    |(RHPVOL << 0))) << 5);
+    i |= (send_two_bytes_to_wm8731(0x0AU, ((HPOR << 4)|(DACMU << 3)
+                                    |(DEEMPH << 1)
+                                    |(ADCHPD << 0))) << 6);
+    i |= (send_two_bytes_to_wm8731(0x08U, ((SIDEATT << 6)|(SIDETONE << 5)
+                                    |(DACSEL << 4)
+                                    |(BYPASS << 3)|(INSEL << 2)|(MUTEMIC << 1)
+                                    |(MICBOOST << 0))) << 7);
+    i |= (send_two_bytes_to_wm8731(0x10U, ((CLKODIV2 << 7)|(CLKIDIV2 << 6)
+                                    |(SR << 2)
+                                    |(BOSR << 1)|(USB << 0))) << 8);
+    i |= (send_two_bytes_to_wm8731(0x12U, (ACTIVE << 0)) << 9);
+
+    return i;
+}
+
+uint8_t SAM3X8EClass::send_two_bytes_to_wm8731(uint8_t byte1, uint8_t byte2)
+{
+    uint16_t i = 0;
+    TWI1_THR = byte1;
+
+    while((TWI1_SR & TXRDY) != TXRDY)
+    {
+        if(i++ > GLOBAL_TIMEOUT)
+        {
+            return 1;
+        }
+    }
+    i = 0;
+
+    TWI1_THR = byte2;
+    
+    while((TWI1_SR & TXRDY) != TXRDY)
+    {
+        if(i++ > GLOBAL_TIMEOUT)
+        {
+            return 1;
+        }
+    }
+    i = 0;
+    
+    TWI1_CR = STOP;
+    
+    while((TWI1_SR & TXCOMP) != TXCOMP)
+    {
+        if(i++ > GLOBAL_TIMEOUT)
+        {
+            return 1;
+        }
+    }
+    
+    if((TWI1_SR & NACK) == NACK)
+    {
+        return 1;
+    }
+    
+    return 0;
+
+}
+
+/*
+    Enables the peripheral clock for SSC.
+*/
+void SAM3X8EClass::init_ssc_clk(void)
+{
+    PMC_WPMR = 0x504D4300U;
+    PMC_PCER0 = PMC_PCSR0 | SSC_PER;
+    PMC_WPMR = 0x504D4301U;
+}
+
+/*
+    Initializes the SSC controller.
+*/
+void SAM3X8EClass::init_ssc(void)
+{
+    init_ssc_clk();
+    setup_pio_for_ssc();
+}
+
+/*
+    The six pins for SSC are configured.
+    
+*/
+void SAM3X8EClass::setup_pio_for_ssc(void)
+{
+    // Peripheral A
+    PIOB_WPMR = (0x50494FU << 8) | (0U << 0);
+    PIOB_PDR = ~PIOB_PSR | (1U << 17);
+    PIOB_PDR = ~PIOB_PSR | (1U << 18);
+    PIOB_PDR = ~PIOB_PSR | (1U << 19);
+    PIOB_PUDR = (1U << 17) | (1U << 18) | (1U << 19) | PIOB_PUSR;
+    PIOB_ABSR |= (0U << 17);
+    PIOB_ABSR |= (0U << 18);
+    PIOB_ABSR |= (0U << 19);
+    PIOB_WPMR = (0x50494FU << 8) | (1U << 0);
+    // Peripheral B
+    PIOA_WPMR = (0x50494FU << 8) | (0U << 0);
+    PIOA_PDR = ~PIOA_PSR | (1U << 14);
+    PIOA_PDR = ~PIOA_PSR | (1U << 15);
+    PIOA_PDR = ~PIOA_PSR | (1U << 16);
+    PIOA_OER = PIOA_OSR | (1U << 16);
+    PIOA_PUDR = (1U << 14) | (1U << 15) | (1U << 16) | PIOA_PUSR;
+    PIOA_ABSR |= (1U << 14);
+    PIOA_ABSR |= (1U << 15);
+    PIOA_ABSR |= (1U << 16);
+    PIOA_WPMR = (0x50494FU << 8) | (1U << 0);
+}
+
+/*
+    Configures SSC for master mode I2S transfer.
+*/
+void SAM3X8EClass::setup_ssc_master_transfer(void)
+{
+    SSC_WPMR = WPKEY | (0U << 0);
+    SSC_CR = SWRST;  // Reset
+    
+    SSC_CR = RXEN | RXDIS | TXEN | TXDIS;
+    SSC_CMR = SSC_DIV; // divider is deaktivated (kept for compatibility)
+    
+    // Clock for receiver and transmitter will both come from RK
+    SSC_RCMR = SSC_RCKS | SSC_RCKO | SSC_RCKI | SSC_RCKG | SSC_RSTART
+                       | SSC_RSTOP | SSC_RSTTDLY | SSC_RPERIOD;
+    
+    SSC_RFMR = SSC_RDATLEN | SSC_RLOOP | SSC_RMSBF | SSC_RDATNB
+                           | SSC_RFSLEN | SSC_RFSOS | SSC_RFSEDGE
+                           | RFSLEN_EXT;
+    
+    // Transmitter Config
+    SSC_TCMR = SSC_TCKS | SSC_TCKI | SSC_TCKO | SSC_TCKG | SSC_TSTART
+                        | SSC_TSTTDLY | SSC_TPERIOD;
+    
+    SSC_TFMR = SSC_TDATLEN | SSC_TDATDEF | SSC_TMSBF | SSC_TDATNB
+                           | SSC_TFSLEN | SSC_TFSOS | SSC_TFSEDGE
+                           | TFSLEN_EXT;
+    
+    SSC_IER = SSC_IETXRDY | SSC_IETXEMPTY | SSC_IERXRDY | SSC_IEOVRUN
+                          | SSC_IECP0 | SSC_IECP1 | SSC_IETXSYN
+                          | SSC_IERXSYN;
+    
+    SSC_IDR = SSC_IDTXRDY | SSC_IDTXEMPTY | SSC_IDRXRDY | SSC_IDOVRUN
+                          | SSC_IDCP0 | SSC_IDCP1 | SSC_IDTXSYN
+                          | SSC_IDRXSYN;
+    
+    SSC_WPMR = WPKEY | (1U << 0);
+}
+
+void SAM3X8EClass::ssc_interrupt_setup(void)
+{
+    INTERRUPT_ENABLE |= (1U << 26);
+    (*((volatile unsigned long *)(0xE000E400 + (6)))) |=
+                                    (uint8_t)(SSC_INTERRUPT_PRIORITY << 16);
+}
+
+void SAM3X8EClass::disable_ssc_interrupt_for_dma(void)
+{
+    INTERRUPT0_DISABLE |= (0U << 26);
+    INTERRUPT0_CLEAR_PENDING = (0xFFFFFFFFU);
+
+}
+
+void SAM3X8EClass::enable_digital_input(void)
+{
+    // Pin 20 - PD7
+    // PIOD: PIO 14
+    PMC_WPMR = WPKEY | 0U;
+    PMC_PCER0 = PMC_PCSR0 | (1U << 14);  // PIO: 14
+    PMC_WPMR = WPKEY | 1U;
+    PIOD_WPMR = PIO_WPMR_KEY | 0U;
+    PIOD_PER = PIOC_PSR | (1U << 7);  // PD7 = Pin 20
+    PIOD_PUDR = (1U << 7);
+    PIOD_ODR = (1U << 7);
+    PIOD_IFER = (1U << 7);
+    PIOD_LSR = (1U << 7);
+    PIOD_DIFSR = (1U << 7);
+    PIOD_WPMR = PIO_WPMR_KEY | 1U;
+    
+}
+
+void SAM3X8EClass::enable_digital_input_interrupts(void)
+{
+    PIOD_WPMR = PIO_WPMR_KEY | 0U;
+    PIOD_IER = PIOD_IMR | (1U << 7);
+    PIOD_WPMR = PIO_WPMR_KEY | 1U;
+    INTERRUPT_ENABLE |= (1U << 14);
+    (*((volatile unsigned long *)(0xE000E400U + (3)))) |=
+                                (uint8_t)(DIG_INP_INTERRUPT_PRIORITY << 16);
+}
+
+void SAM3X8EClass::enable_adc_input(void)
+{
+    // Pin 81 - Signal: AD4 - I/O line: PA22 - Peripheral: X1 - Channel: CH4
+    // ADC: PID 37
+    // 10 bit resolution
+    // Interrupt ID: 37
+  
+    /*
+        Enable Power Management Controller and Clock for ADC
+    */
+    PMC_WPMR = WPKEY | 0U;
+    PMC_PCER1 = PMC_PCSR1 | (1U << 5);  // PID: 37
+    PMC_WPMR = WPKEY | 1U;
+    PIOD_WPMR = PIO_WPMR_KEY | 0U;
+    
+    PIOA_WPMR = PIO_WPMR_KEY | (0U << 0);
+    PIOA_PDR = (1U << 22);
+    PIOA_ODR = (1U << 22);
+    PIOA_PUDR = (1U << 22);
+    PIOA_WPMR = PIO_WPMR_KEY | (1U << 0);
+
+    //ADC Reset
+    ADC_CR = (1U << 0);
+    
+    ADC_WPMR = ADC_WPMR_KEY | (0U << 0);
+    //ADC_CHER enable ADC input and assign pin to adc
+    ADC_CHER = ADC_CHSR | (1U << 4);
+    
+    //ADC config
+    ADC_MR = ADC_TRGEN | ADC_TRGSEL | ADC_LOWRES | ADC_SLEEP | ADC_FWUP
+                       | ADC_FREERUN | ADC_PRESCAL | ADC_STARTUP
+                       | ADC_SETTLING | ADC_ANACH | ADC_TRACKTIM
+                       | ADC_TRANSFER | ADC_USEQ;
+
+    //ADC Start
+    ADC_CR = (1U << 1);
+
+    ADC_WPMR = ADC_WPMR_KEY | (1U << 0);
+}
+
+void SAM3X8EClass::enable_adc_input_interrupts(void)
+{
+
+}
+
+void SAM3X8EClass::configure_dma(uint8_t channel, uint8_t sod,
+                                 uint32_t* src, uint32_t* dst,
+                                 uint32_t btsize,
+                                 uint8_t src_dev,
+                                 uint8_t dest_dev)
+{
+    uint32_t read_dummy = 0;
+    uint8_t src_per = 0;
+    uint8_t dst_per = 0;
+    uint8_t src_h2sel = 0;
+    uint8_t dst_h2sel = 0;
+    uint8_t fc = 0;
+    
+    if(src_dev == 9)
+    {
+        src_per = 0;
+        src_h2sel = 0;
+    }
+    else
+    {
+        src_per = src_dev;
+        src_h2sel = 1;
+    }
+    
+    if(dest_dev == 9)
+    {
+        dst_per = 0;
+        dst_h2sel = 0;
+    }
+    else
+    {
+        dst_per = dest_dev;
+        dst_h2sel = 1;
+    }
+    
+    if(src_dev == 9)
+    {
+        if(dest_dev == 9)
+        {
+            fc = 0x0U;
+        }
+        else
+        {
+            fc = 0x1U;
+        }
+    }
+    else
+    {
+        if(dest_dev == 9)
+        {
+            fc = 0x2U;
+        }
+        else
+        {
+            fc = 0x3U;
+        }
+      
+    }
+    
+    /*
+        pmc_enable_periph_clk(ID_DMAC);
+    */
+    PMC_WPMR = WPKEY | 0U;
+    PMC_PCER1 = PMC_PCSR1 | (1U << 7);  // PID: 39 (DMA)
+    PMC_WPMR = WPKEY | 1U;
+    
+    /*
+        dmac_init(DMAC);
+        = disable DMA
+    */
+    DMAC_WPMR = DMAC_WPM_KEY | (0U << 0);
+    DMAC_EN = 0U;
+    
+    /*
+        dmac_set_priority_mode(DMAC, DMAC_PRIORITY_ROUND_ROBIN);
+    */
+    DMAC_GCFG = DMAC_ARB_CFG; // Round Robin
+    
+    enable_dma();
+    
+    DMAC_CFG0(channel) = DMAC_CFG0_SOD(sod) | DMAC_CFG0_AHB_PROT
+                                            | DMAC_CFG0_FIFOCFG
+                                            | (uint8_t)src_per
+                                            | ((uint8_t)dst_per << 4)
+                                          | ((uint8_t)(0x1U & dst_h2sel) << 13)
+                                          | ((uint8_t)(0x1U & src_h2sel) << 9);
+    
+    /* Clear any pending interrupts */
+    read_dummy = DMAC_EBCISR;
+    
+    DMAC_SADDR0(channel) = (uint32_t)src;
+    DMAC_DADDR0(channel) = (uint32_t)dst;
+    
+    /*
+        dmac_channel_set_descriptor_addr(p_dmac, ul_num, 0);
+    */
+    DMAC_DSCR0 = (0U << 2);
+    
+    /*
+        dmac_channel_set_ctrlA(p_dmac, ul_num, p_desc->ul_ctrlA);
+    */
+    DMAC_CTRLA0(channel) = DMAC_CTRLA0_DST_WIDTH | DMAC_CTRLA0_SRC_WIDTH
+                                        | (0x0000FFFFU & (btsize));
+    
+    /*
+        dmac_channel_set_ctrlB
+    */
+    DMAC_CTRLB0(channel) = DMAC_CTRLB0_SRC_DSCR | DMAC_CTRLB0_DST_DSCR
+                                       | ((0x7U << 21) & (fc << 21))
+                                       | DMAC_CTRLB0_SRC_INCR
+                                       | DMAC_CTRLB0_DST_INCR;
+    
+    DMAC_WPMR = DMAC_WPM_KEY | (1U << 0);    
+
+}
+
+void SAM3X8EClass::enable_dma(void)
+{
+    /*
+        dmac_enable(DMAC);
+    */
+    DMAC_WPMR = DMAC_WPM_KEY | (0U << 0);
+    DMAC_EN = 1U;
+    DMAC_WPMR = DMAC_WPM_KEY | (1U << 0);
+}
+
+void SAM3X8EClass::enable_dma_interrupts(uint8_t channel)
+{
+    DMAC_WPMR = DMAC_WPM_KEY | (0U << 0);
+    DMAC_EBCIER = DMAC_EBCISR | (1U << channel);
+    DMAC_WPMR = DMAC_WPM_KEY | (1U << 0);
+    
+    INTERRUPT1_ENABLE |= (1U << 7);  // ISR 37 (PID 39?!)
+    
+    (*((volatile unsigned long *)(0xE000E400 + (9)))) |=
+                                (uint8_t)(DMAC_INTERRUPT_PRIORITY << 24);
+
+}
+
+void SAM3X8EClass::enable_dma_channel(uint8_t channel)
+{
+    /*
+        dmac_channel_enable(DMAC, DMA_CH);
+    */
+    DMAC_WPMR = DMAC_WPM_KEY | (0U << 0);
+    DMAC_CHER = (0x0000003FU & (1U << channel));
+    DMAC_WPMR = DMAC_WPM_KEY | (1U << 0);
+}
+
+void SAM3X8EClass::disable_watchdog_timer(void)
+{
+    (*((volatile unsigned long *)0x400E1A54U)) = (1U << 15);
+}
+
+void SAM3X8EClass::set_dma_btsize(uint8_t channel, uint32_t btsize)
+{
+    DMAC_WPMR = DMAC_WPM_KEY | (0U << 0);
+    DMAC_EN = 0U;
+    DMAC_CTRLA0(channel) = DMAC_CTRLA0(channel) | (0x0000FFFFU & (btsize));
+    enable_dma();
+    DMAC_WPMR = DMAC_WPM_KEY | (1U << 0);
+
+
+}
+
+// Create object
+SAM3X8EClass SAM3X8E = SAM3X8EClass();
